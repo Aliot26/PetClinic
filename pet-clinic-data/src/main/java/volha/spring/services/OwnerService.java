f50d3b300b2f3b3fcf53a04a9b96b63ae1690865
Aliot26 <olgakochanowa@gmail.com>
2020-01-05 20:36:33 +0100

    created services interfaces

---
+++ pet-clinic-data/src/main/java/volha/spring/services/OwnerService.java
@@ -0,0 +1,16 @@
+package volha.spring.services;
+
+import volha.spring.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}

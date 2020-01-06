5ae3fd8cb63c43f0b12d87a1487413a9f7e2ba7c
Aliot26 <olgakochanowa@gmail.com>
2020-01-06 14:18:55 +0100

    implemented map services

---
+++ pet-clinic-data/src/main/java/volha/spring/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package volha.spring.services.map;
+
+import volha.spring.model.Owner;
+import volha.spring.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> finfAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}

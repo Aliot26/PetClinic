742481ac18878b6b978472d0049e1104d21c8b7e
Aliot26 <olgakochanowa@gmail.com>
2020-01-06 19:48:41 +0100

    created vets and owners index.pages and controllers

---
+++ pet-clinic-web/src/main/java/volha/spring/petclinic/comtrollers/OwnersController.java
@@ -0,0 +1,12 @@
+package volha.spring.petclinic.comtrollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnersController {
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    public String listVets(){
+        return "owners/index";
+    }
+}

ac727afc30a5e09e37c1a9737bb8a7bd5997dbe2
Aliot26 <olgakochanowa@gmail.com>
2020-01-17 19:27:12 +0100

    Created Speciality Entity

--- pet-clinic-data/src/main/java/volha/spring/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/volha/spring/petclinic/model/Vet.java
@@ -1,7 +1,18 @@
 package volha.spring.petclinic.model;
 
+import java.util.Set;
+
 /**
  * created 05.01.2020
  */
 public class Vet extends Person {
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }

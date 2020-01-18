2bf133f22be51712c9c1e00b39e1e2ee4864ba4e
Aliot26 <olgakochanowa@gmail.com>
2020-01-18 21:46:24 +0100

    Created  Owner Entity

--- pet-clinic-data/src/main/java/volha/spring/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/volha/spring/petclinic/model/Pet.java
@@ -1,15 +1,27 @@
 package volha.spring.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * created 05.01.2020
  */
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthData;
 
     public String getName() {

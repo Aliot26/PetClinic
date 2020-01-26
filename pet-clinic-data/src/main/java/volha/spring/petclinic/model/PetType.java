16e7f46c3e1e6ca880bb1f34f8cdbb62d53770d8
Aliot26 <olgakochanowa@gmail.com>
2020-01-26 18:14:59 +0100

    added lombok annotations to model pet-clinic-data

--- pet-clinic-data/src/main/java/volha/spring/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/volha/spring/petclinic/model/PetType.java
@@ -1,5 +1,7 @@
 package volha.spring.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -7,19 +9,16 @@ import javax.persistence.Table;
 /**
  * created 05.01.2020
  */
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
 
     @Column(name = "name")
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
 
--- pet-clinic-data/src/main/java/volha/spring/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/volha/spring/petclinic/model/Vet.java
@@ -1,5 +1,7 @@
 package volha.spring.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
@@ -7,6 +9,11 @@ import java.util.Set;
 /**
  * created 05.01.2020
  */
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -17,11 +24,4 @@ public class Vet extends Person {
             inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
 }

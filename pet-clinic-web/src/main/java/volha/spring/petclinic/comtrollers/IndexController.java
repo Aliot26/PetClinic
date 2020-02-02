b3458cb4d3aa1a455bbaca7a4332a79a13bfca11
Aliot26 <olgakochanowa@gmail.com>
2020-02-02 12:26:22 +0100

    Added tests to IndexController with MockMVC

--- pet-clinic-web/src/main/java/volha/spring/petclinic/comtrollers/IndexController.java
+++ pet-clinic-web/src/main/java/volha/spring/petclinic/comtrollers/IndexController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping({"/", "", "index", "index.html"})
+    @RequestMapping({"/", "", "/index", "/index.html"})
     public String index(){
         return "index";
     }
 
     @RequestMapping("/oups")
-    public String notimplement(){
+    public String notimplements(){
         return "notimplement";
     }
 }
